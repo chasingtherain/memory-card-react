6c7b1630efb2cb4a7a6ff2e949444d710aa29362
ChasingRain <junpeng94@gmail.com>
2022-06-29 13:19:39 +0800

    added flip effect, refactored match logic

--- src/App.js
+++ src/App.js
@@ -3,31 +3,58 @@ import Card from './components/Card'
 import './App.css'
 
 const cardImages = [
-  {"src": "/img/scroll-1.png", "category": 1},
-  {"src": "/img/helmet-1.png", "category": 2},
-  {"src": "/img/potion-1.png", "category": 3},
-  {"src": "/img/ring-1.png", "category": 4},
-  {"src": "/img/shield-1.png", "category": 5},
-  {"src": "/img/sword-1.png", "category": 6}
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/shield-1.png"},
+  {"src": "/img/sword-1.png"}
 ]
 function App() {
   const [cards, setCards] = useState([])
-  const [prevCard, setPrevCard] = useState("")
-  const [currentCard, setCurrentCard] = useState("")
-  const [flippedCardCount, setFlippedCardCount] = useState(0)
+  const [prevCard, setPrevCard] = useState(null)
+  const [currentCard, setCurrentCard] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
+  // setting card choice
+  const handleChoice = (card) => {
+      (currentCard) ? setPrevCard(card) : setCurrentCard(card)      
+  }
 
+  // update matched status if there is a match
+  const updateMatchedStatus = () => {
+    setCards((prevState) => prevState.map(card => {
+      if (card.src === prevCard.src) {
+        return {...card, matched: true}
+      }
+      return card
+    }))
+  }
+
+  // compare between two cards
   useEffect(()=>{
-    if(prevCard === currentCard){
-      console.log(prevCard,currentCard,cards);
-      const unaffectedArray = cards.filter(card => card.category !== +prevCard)
-      const matchedId = cards.filter(card => card.category === +prevCard)
-      matchedId.forEach((card)=> card.matched = true)
-      console.log(matchedId);
-      setCards(unaffectedArray.concat(matchedId))
-      console.log(cards);
-    } 
+    if(prevCard && currentCard){
+      setDisabled(true)
+      if(prevCard.src === currentCard.src){
+        console.log("cards matched", cards);
+        updateMatchedStatus()
+        resetTurn()
+      } 
+      else{
+        console.log("cards do not match");
+      setTimeout(() => {
+        resetTurn()
+      },1200)
+      }
+    }
   },[prevCard,currentCard])
+
+
+  const resetTurn = () => {
+    setPrevCard(null)
+    setCurrentCard(null)
+    setDisabled(false)
+  }
 
   const shuffleCards = () => {
     const shuffledCard = [...cardImages,...cardImages]
@@ -36,8 +63,6 @@ function App() {
       // console.log(shuffledCard);
       setCards(shuffledCard)
   }
-
-  // const compareSelection
 
   return (
     <div className="App">
@@ -49,11 +74,10 @@ function App() {
             key = {card.id}
             card = {card}
             currentCard={currentCard}
-            setCurrentCard={setCurrentCard}
             prevCard={prevCard}
-            setPrevCard={setPrevCard}
-            flippedCardCount = {flippedCardCount}
-            setFlippedCardCount = {setFlippedCardCount}
+            handleChoice = {handleChoice}
+            flipped = {card === prevCard || card === currentCard || card.matched}
+            disabled = {disabled}
           />
         )}
       </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,35 +1,16 @@
-import React, { useState } from 'react'
+import "./Card.css"
 
-function Card({card, currentCard, prevCard, setCurrentCard, setPrevCard,flippedCardCount, setFlippedCardCount}) {
-    const [userFlips, setUserFlips] = useState(false)
+function Card({card, flipped, handleChoice, disabled}) {
 
-    const handleCardFlip = (event) => {
-        if(currentCard && !userFlips) setPrevCard(currentCard)
-        setCurrentCard(event.target.id);
-        console.log(prevCard,currentCard);
-        if(flippedCardCount < 2) {
-            setUserFlips(true)
-            setFlippedCardCount((prevState) => prevState+1)
-            setTimeout(() => {
-                setUserFlips(false)
-                setPrevCard("")
-                setCurrentCard("")
-                setFlippedCardCount((prevState) => prevState-1)
-            },1500)
-            // console.log("current count: ", flippedCardCount);
-        }
-    
+    const handleClick = () => {
+        if(!disabled) handleChoice(card)
     }
 
     return (
     <div className='card' key={card.id}>
-        <div onClick={handleCardFlip} >
-            {
-                (card.matched) ? <img className='front' src={card.src} id={card.id} alt="card front"/>
-                : ((userFlips) ? 
-                        <img className='front' src={card.src} alt="card front"/>
-                    :   <img className='back' src="/img/cover.png" id={card.category} alt="card back"/>)
-            }
+        <div className={flipped ? "flipped" : ""}>
+                <img className='front' src={card.src} alt="card front"/>
+                <img className='back' onClick={handleClick} src="/img/cover.png" alt="card back"/>
         </div>
     </div>
     )
